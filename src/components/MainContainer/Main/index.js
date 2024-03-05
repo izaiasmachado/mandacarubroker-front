52b2a12108e9705221453e57c6dcc62964f9c94a
iriscoxta <irisccosta55@gmail.com>
2024-03-05 19:14:37 -0300

    :sparkles: feat: Main Component componente central da página

---
+++ src/components/MainContainer/Main/index.js
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+import LeftConteiner from "./LeftConteiner";
+import RightConteiner from "./RightConteiner";
+
+const Main = () => {
+  return (
+    <S.Main>
+        <LeftConteiner />
+        <RightConteiner />
+    </S.Main>
+  );
+};
+
+export default Main;
