8a3e026166a076881e62457016c5bf86a28ccda4
iriscoxta <irisccosta55@gmail.com>
2024-03-05 19:14:03 -0300

    :sparkles: feat: Hero Component

---
+++ src/components/MainContainer/Hero/index.js
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+const Hero = () => {
+  return (
+    <S.HeroWrapper>
+        <S.HeroText>Gerencie suas ações <span> ;)</span></S.HeroText>
+        <S.Line />
+    </S.HeroWrapper>
+  );
+};
+
+export default Hero;
