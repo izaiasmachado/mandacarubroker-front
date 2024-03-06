d0fc1f30896574c67e8d432786cb04d4183cc379
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-06 13:26:35 -0300

    feat: login component

---
+++ src/app/login/page.js
@@ -0,0 +1,43 @@
+"use use client";
+
+import { Background, Input, SubmitButton } from "@/styles/global";
+import {
+  LoginConteiner,
+  LoginWrapper,
+  LoginFormContainer,
+  PartnerLogo,
+  PartnerLogosContainer,
+  LoginSubmitionContainer,
+  DontHaveAccountMessage,
+} from "@/styles/login";
+
+export default function Login() {
+  return (
+    <>
+      <Background />
+      <LoginWrapper>
+        <LoginConteiner>
+          <PartnerLogosContainer>
+            <PartnerLogo>
+              <img src="img/logo-mandacaru.png" alt="Logo Mandacaru.dev" />
+            </PartnerLogo>
+            <PartnerLogo>
+              <img src="img/b3-icone-white.png" alt="Logo B3" />
+            </PartnerLogo>
+          </PartnerLogosContainer>
+
+          <LoginFormContainer>
+            <Input type="text" placeholder="Usuário"></Input>
+            <Input type="password" placeholder="Senha"></Input>
+          </LoginFormContainer>
+          <LoginSubmitionContainer>
+            <SubmitButton>Entrar</SubmitButton>
+            <DontHaveAccountMessage>
+              Não tem uma conta? <a href="/cadastro">Cadastre-se</a>
+            </DontHaveAccountMessage>
+          </LoginSubmitionContainer>
+        </LoginConteiner>
+      </LoginWrapper>
+    </>
+  );
+}
