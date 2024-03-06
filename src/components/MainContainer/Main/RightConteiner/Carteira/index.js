4605ce58d6c598b43409221b7cde513690fc9829
iriscoxta <irisccosta55@gmail.com>
2024-03-05 23:34:44 -0300

    :sparkles: feat: mock components

---
+++ src/components/MainContainer/Main/RightConteiner/Carteira/index.js
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+const Carteira = () => {
+  
+  return (
+   <h1>Carteira</h1>
+  );
+};
+
+export default Carteira;
---
+++ src/components/MainContainer/Main/RightConteiner/Historico/index.js
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+const Historico = () => {
+  
+  return (
+   <h1>Histórico</h1>
+  );
+};
+
+export default Historico;
