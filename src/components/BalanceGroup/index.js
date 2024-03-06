5bff4a899dc1106ee262d3246f20e86ca03f5efa
iriscoxta <irisccosta55@gmail.com>
2024-03-06 19:20:37 -0300

    :boom: remove wallet icon

--- src/components/BalanceGroup/index.js
+++ src/components/BalanceGroup/index.js
@@ -1,10 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import { faWallet, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import Button from "../UI/Button";
 
 import * as S from "./styles";
 
@@ -17,19 +15,14 @@ const Balance = () => {
 
   return (
     <S.Balance>
-      <S.BalanceLeftContent>
-        <S.Icon width={21} height={18}>
-          <FontAwesomeIcon icon={faWallet} />
-        </S.Icon>
-      </S.BalanceLeftContent>
       <S.BalanceRightcontent>
-            <S.Currency>
-            <h3>Saldo</h3>
-            <span>{showPassword ? <hr></hr> : "R$ 0,00"}</span>
-            </S.Currency>
-            <S.Icon width={15} height={18} onClick={togglePasswordVisibility}>
-            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-            </S.Icon>
+        <S.Currency>
+          <h3>Saldo</h3>
+          <span>{showPassword ? <hr></hr> : "R$ 0,00"}</span>
+        </S.Currency>
+        <S.Icon width={15} height={18} onClick={togglePasswordVisibility}>
+          <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+        </S.Icon>
       </S.BalanceRightcontent>
     </S.Balance>
   );
