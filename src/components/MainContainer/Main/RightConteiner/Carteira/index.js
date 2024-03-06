f3ad840599f8e63c3b086ab602628c981930797e
iriscoxta <irisccosta55@gmail.com>
2024-03-06 00:25:23 -0300

    :sparkles: feat: Carteira Component

--- src/components/MainContainer/Main/RightConteiner/Carteira/index.js
+++ src/components/MainContainer/Main/RightConteiner/Carteira/index.js
@@ -1,9 +1,75 @@
+import React, { useState } from "react";
 import * as S from "./styles";
+import Button from "@/components/UI/Button";
 
 const Carteira = () => {
-  
+  const [value, setValue] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [transactionType, setTransactionType] = useState("");
+
+  const handleValueChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const handleTransactionTypeChange = (e) => {
+    setTransactionType(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Value:", value);
+    console.log("Quantity:", quantity);
+    console.log("Transaction Type:", transactionType);
+  };
+
   return (
-   <h1>Carteira</h1>
+    <S.Container>
+      <form onSubmit={handleSubmit}>
+        <S.FormContent>
+          <S.InputGroup>
+            <S.InputWrapper>
+              <S.Label>Valor:</S.Label>
+              <S.Input
+                placeholder="R$: 0,00"
+                type="text"
+                value={value}
+                onChange={handleValueChange}
+              />
+            </S.InputWrapper>
+          </S.InputGroup>
+          <S.InputWrapper>
+            <S.Label>Tipo de Transação:</S.Label>
+            <S.RadioWrapper>
+              <S.RadioLabel>
+                <S.RadioButton
+                  type="radio"
+                  name="transactionType"
+                  value="deposito"
+                  checked={transactionType === "deposito"}
+                  onChange={handleTransactionTypeChange}
+                />
+                <S.Label>Depósito</S.Label>
+              </S.RadioLabel>
+              <S.RadioLabel>
+                <S.RadioButton
+                  type="radio"
+                  name="transactionType"
+                  value="saque"
+                  checked={transactionType === "saque"}
+                  onChange={handleTransactionTypeChange}
+                />
+                <S.Label>Saque</S.Label>
+              </S.RadioLabel>
+            </S.RadioWrapper>
+          </S.InputWrapper>
+        </S.FormContent>
+        <Button text="Confirmar" width={"100%"} height={30} />
+      </form>
+    </S.Container>
   );
 };
 
