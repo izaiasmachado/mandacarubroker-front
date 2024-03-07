731c763924835e5b14e93e0adb5231cbdc955c4f
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-07 16:44:05 -0300

    feat: change login to root and redirect to dashboard if needed

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,110 @@
 "use client";
 
-import styled from "styled-components";
+import {
+  Background,
+  Input,
+  SubmitButton,
+  ValidationErrorMessage,
+} from "@/styles/global";
 
-const RedirectText = styled.h1`
-  color: #fff;
-  font-size: 2.4rem;
-  padding: 20px 15px;
-`;
+import {
+  LoginConteiner,
+  LoginWrapper,
+  LoginFormContainer,
+  PartnerLogo,
+  PartnerLogosContainer,
+  LoginSubmitionContainer,
+  DontHaveAccountMessage,
+  ApplicationHeroTitleContainer,
+  ApplicationHeroTitle,
+  ApplicationHeroTitleFirstLine,
+  ApplicationHeroTitleSecondLine,
+} from "@/styles/login";
 
-export default function Dashboard() {
-  if (typeof window === "undefined") return <></>;
+import Link from "next/link";
+import { useState } from "react";
 
-  if (!localStorage || !localStorage.getItem("access_token")) {
-    window.location.href = "/login";
-    return (
-      <div>
-        <RedirectText>Redirecionando para tela de login...</RedirectText>
-      </div>
-    );
+import { api } from "@/lib/api";
+
+export default function Login() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [credentialsError, setCredentialsError] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/auth/login", {
+        username: user,
+        password: password,
+      });
+      if (credentialsError) setCredentialsError(false);
+      localStorage.setItem("access_token", response.data.token);
+      window.location.href = "/dashboard";
+    } catch (error) {
+      setCredentialsError(true);
+    }
+  };
+
+  if (
+    typeof window !== "undefined" &&
+    localStorage &&
+    localStorage.getItem("access_token")
+  ) {
+    window.location.href = "/dashboard";
+    return <></>;
   }
 
-  window.location.href = "/dashboard";
   return (
-    <div>
-      <RedirectText>Redirecionando para tela de dashboard...</RedirectText>
-    </div>
+    <>
+      <Background />
+      <LoginWrapper>
+        <ApplicationHeroTitleContainer>
+          <ApplicationHeroTitle>
+            <ApplicationHeroTitleFirstLine>
+              MANDACARU
+            </ApplicationHeroTitleFirstLine>
+            <ApplicationHeroTitleSecondLine>
+              BROKER
+            </ApplicationHeroTitleSecondLine>
+          </ApplicationHeroTitle>
+        </ApplicationHeroTitleContainer>
+        <LoginConteiner>
+          <PartnerLogosContainer>
+            <PartnerLogo>
+              <img src="img/logo-mandacaru.png" alt="Logo Mandacaru.dev" />
+            </PartnerLogo>
+            <PartnerLogo>
+              <img src="img/b3-icone-white.png" alt="Logo B3" />
+            </PartnerLogo>
+          </PartnerLogosContainer>
+
+          <LoginFormContainer>
+            <Input
+              type="text"
+              placeholder="Usuário"
+              onChange={(e) => setUser(e.target.value)}
+            ></Input>
+            <Input
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+            ></Input>
+
+            {credentialsError && (
+              <ValidationErrorMessage>
+                Usuário ou senha incorretos
+              </ValidationErrorMessage>
+            )}
+          </LoginFormContainer>
+          <LoginSubmitionContainer>
+            <SubmitButton onClick={handleLogin}>Entrar</SubmitButton>
+            <DontHaveAccountMessage>
+              Não tem uma conta? <Link href="/cadastro">Cadastre-se</Link>
+            </DontHaveAccountMessage>
+          </LoginSubmitionContainer>
+        </LoginConteiner>
+      </LoginWrapper>
+    </>
   );
 }
