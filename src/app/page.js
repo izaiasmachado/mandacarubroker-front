0a56763d9b410238e1509291091114bc9c93faa7
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-07 16:30:17 -0300

    feat: do redirects from root

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
+"use client";
+
+import styled from "styled-components";
+
+const RedirectText = styled.h1`
+  color: #fff;
+  font-size: 2.4rem;
+  padding: 20px 15px;
+`;
+
 export default function Dashboard() {
+  if (typeof window === "undefined") return <></>;
+
+  if (!localStorage || !localStorage.getItem("access_token")) {
+    window.location.href = "/login";
+    return (
+      <div>
+        <RedirectText>Redirecionando para tela de login...</RedirectText>
+      </div>
+    );
+  }
+
+  window.location.href = "/dashboard";
   return (
-    <>
-    </>
+    <div>
+      <RedirectText>Redirecionando para tela de dashboard...</RedirectText>
+    </div>
   );
 }
