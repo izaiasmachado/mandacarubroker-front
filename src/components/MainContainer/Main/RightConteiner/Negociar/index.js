a43df13b6e7154e362507d8bce2ed208bb765468
iriscoxta <irisccosta55@gmail.com>
2024-03-07 20:48:03 -0300

    :sparkles: feat: Stocks details on Negociar component

--- src/components/MainContainer/Main/RightConteiner/Negociar/index.js
+++ src/components/MainContainer/Main/RightConteiner/Negociar/index.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styles";
 import Button from "@/components/UI/Button";
 
-const Negociar = () => {
+const Negociar = ({ selectedStockData }) => {
   const [value, setValue] = useState("");
   const [quantity, setQuantity] = useState("");
   const [transactionType, setTransactionType] = useState("compra");
+
+  useEffect(() => {
+    if (selectedStockData) {
+      setValue(selectedStockData.value);
+    }
+  }, [selectedStockData]);
 
   const handleValueChange = (e) => {
     setValue(e.target.value);
@@ -31,52 +37,59 @@ const Negociar = () => {
       <form onSubmit={handleSubmit}>
         <S.FormContent>
           <S.InputGroup>
-            <S.InputWrapper>
-              <S.Label>Valor:</S.Label>
-              <S.Input
-                placeholder="R$: 0,00"
-                type="text"
-                value={value}
-                onChange={handleValueChange}
-              />
-            </S.InputWrapper>
-            <S.InputWrapper>
-              <S.Label>Quantidade:</S.Label>
-              <S.Input
-                placeholder="0"
-                value="0"
-                type="number"
-                onChange={handleQuantityChange}
-              />
-            </S.InputWrapper>
+            {selectedStockData ? (
+              <>
+                <S.InputWrapper>
+                  <S.Label>Stock selecionada:</S.Label>
+                  <S.Input
+                    type="text"
+                    value={selectedStockData.symbol}
+                    onChange={handleValueChange}
+                  />
+                </S.InputWrapper>
+                <S.InputWrapper>
+                  <S.Label>Quantidade:</S.Label>
+                  <S.Input
+                    placeholder="0"
+                    value={quantity}
+                    type="number"
+                    onChange={handleQuantityChange}
+                  />
+                </S.InputWrapper>
+                {/* Se a quantidade for maior que 0, exibe as informações */}
+                {quantity > 0 ? (
+                  <>
+                    <S.Label>Informações:</S.Label>
+                    <S.Resumo>
+                      <span>
+                        Você está comprando{" "}
+                        {(quantity * selectedStockData.price).toLocaleString(
+                          "pt-BR",
+                          {
+                            style: "currency",
+                            currency: "BRL",
+                          }
+                        )}{" "}
+                        em ações de {selectedStockData.companyName}.
+                      </span>
+                    </S.Resumo>
+                    <S.Label>Deseja confirmar a transação?</S.Label>
+                  </>
+                ) : (
+                  <S.Label>Selecione a quantidade mínima</S.Label>
+                )}
+              </>
+            ) : (
+              <S.Label>Não há Stock selecionada</S.Label>
+            )}
           </S.InputGroup>
-          <S.InputWrapper>
-            <S.Label>Tipo de Transação:</S.Label>
-            <S.RadioWrapper>
-              <S.RadioLabel>
-                <S.RadioButton
-                  type="radio"
-                  name="transactionType"
-                  value="compra"
-                  checked={transactionType === "compra"}
-                  onChange={handleTransactionTypeChange}
-                />
-                <S.Label>Compra</S.Label>
-              </S.RadioLabel>
-              <S.RadioLabel>
-                <S.RadioButton
-                  type="radio"
-                  name="transactionType"
-                  value="venda"
-                  checked={transactionType === "venda"}
-                  onChange={handleTransactionTypeChange}
-                />
-                <S.Label>Venda</S.Label>
-              </S.RadioLabel>
-            </S.RadioWrapper>
-          </S.InputWrapper>
         </S.FormContent>
-        <Button text="Confirmar" width={"100%"} height={"30px"} clicked={false} />
+        <Button
+          text="Confirmar"
+          width={"100%"}
+          height={"30px"}
+          clicked={false}
+        />
       </form>
     </S.Container>
   );
