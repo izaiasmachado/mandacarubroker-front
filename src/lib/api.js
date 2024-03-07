195224003a52d78a36e4eba1df6e483acb2e74ed
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-07 07:31:08 -0300

    hotfix: axios prerendering on server

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,10 +4,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.mandacarubroker.com.br",
-  headers: {
-    Authorization:
-      localStorage && localStorage.getItem("access_token")
-        ? localStorage.getItem("access_token")
-        : "",
-  },
 });
