ed89f4d9c04800cd897cd3af7e161d7cd85a8ea9
iriscoxta <irisccosta55@gmail.com>
2024-03-07 14:29:55 -0300

    :lock: feat: redirecting unauthenticated user to login

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,12 @@
+'use client'
+
 import NavBar from "@/components/NavBar";
 import MainGroup from "@/components/MainContainer";
 
 export default function Home() {
+  if (!localStorage.getItem('access_token')) {
+    window.location.href = "/login";
+  }
   return (
     <>
       <NavBar />
@@ -9,4 +14,3 @@ export default function Home() {
     </>
   );
 }
-
