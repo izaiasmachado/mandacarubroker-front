d2d8a7bde64b53170506fd61162128eb8fc292a0
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-08 16:23:21 -0300

    feat: change negotiation tables styles (#15)

---
+++ src/components/UI/TablePortifolio/stock.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import * as S from "./styles";
+
+export default function StockRow({
+  flip,
+  selectedStock,
+  data,
+  handleRadioChange,
+  totalShares,
+  positionValue,
+}) {
+  const priceToReal = (price) => {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  };
+
+  const stockPriceReal = priceToReal(data.price);
+  const positionValueReal = priceToReal(positionValue);
+
+  return (
+    <S.TableRow flip={flip} onClick={() => handleRadioChange(data)}>
+      <S.TableCell align={true}>
+        <input
+          type="radio"
+          name="selectedStock"
+          value={data.id}
+          checked={selectedStock.id === data.id}
+        />
+      </S.TableCell>
+      <S.TableCell>
+        {data.symbol} - {data.companyName}
+      </S.TableCell>
+      <S.TableCell align>{stockPriceReal}</S.TableCell>
+      <S.TableCell align>{totalShares}</S.TableCell>
+      <S.TableCell>{positionValueReal}</S.TableCell>
+    </S.TableRow>
+  );
+}
