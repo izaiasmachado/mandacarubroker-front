887a31f446f186bfadb8d5185dbefc13423efbc5
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-08 12:30:11 -0300

    feat: load balance with user

--- src/components/BalanceGroup/index.js
+++ src/components/BalanceGroup/index.js
@@ -2,13 +2,15 @@
 
 import { useState } from "react";
 import { useBalance } from "@/contexts/BalanceContext";
+import { useUser } from "@/contexts/UserContext";
 
 import { FaWallet, FaEye, FaEyeSlash } from "react-icons/fa6";
 import * as S from "./styles";
 
 const Balance = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
   const { balance } = useBalance();
+  const { isLoading } = useUser();
 
   const balanceToLocalePtBr = (b) => {
     return Number(b).toLocaleString("pt-BR", {
@@ -21,13 +23,15 @@ const Balance = () => {
     setShowPassword(!showPassword);
   };
 
+  const balanceString = isLoading ? "..." : balanceToLocalePtBr(balance);
+
   return (
     <S.Balance>
       <S.BalanceRightcontent>
         <FaWallet size={25} color={"#fff"} />
         <S.Currency>
           <h3>Saldo</h3>
-          <span>{showPassword ? balanceToLocalePtBr(balance) : <hr></hr>}</span>
+          <span>{showPassword ? balanceString : <hr></hr>}</span>
         </S.Currency>
         <S.Icon onClick={togglePasswordVisibility}>
           {showPassword ? (
