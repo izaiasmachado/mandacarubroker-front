a510c7bac55ca2db7095436dee0af2d0d936e6bd
iriscoxta <irisccosta55@gmail.com>
2024-03-08 18:13:53 -0300

    :lipstick: style: increasing container size

--- src/components/MainContainer/Main/styles.js
+++ src/components/MainContainer/Main/styles.js
@@ -7,7 +7,7 @@ export const Main = styled.div`
     display: flex;
     background-color: #011229;
     width: 90%;
-    height: 70%;
+    height: 80%;
     border-radius: 5px;
     padding: 25px;
 `;
