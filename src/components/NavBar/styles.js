4483197dc661b5f87f457eb76f14a115959e33e8
iriscoxta <irisccosta55@gmail.com>
2024-03-08 15:45:21 -0300

    :lipstick: style: moving profile to NavBar and changing logo

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -40,3 +40,45 @@ export const LogoTextSubtitle = styled.span`
 export const LogoTextContainer = styled.div`
   margin-left: 0.55rem;
 `;
+
+export const ContainerProfile = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1rem;
+  width: 15rem;
+  height: 4rem;
+`;
+
+export const Icon = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+`;
+export const ContainerUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  border: solid 2px #ff8c1f;
+  background-color: white;
+`;
+
+export const InformationUser = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Name = styled.span`
+  font-weight: 500;
+  color: white;
+  font-size: 1.4rem;
+  
+`;
+
+export const UserName = styled.span`
+  font-weight:700;
+  color: #FF8C1F;
+  font-size: 1.2rem;
+`;
